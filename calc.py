37db863d7269e898e56316b32f701eed0d207f6e
Stefan Reifberger <sterei@outlook.com>
2024-03-30 11:10:36 +0000

    Rewrote function for building dataset of derivations and added explanations to the fuctions that implement intuitionistic propositional logic in calc.py.

--- calc.py
+++ calc.py
@@ -1,77 +1,91 @@
+# Import libraries
 import numpy as np
 import random as rd
 from random import seed, randint, choice, shuffle
 
-# Calculus 1
+# Intuitionistic propositional logic
 ## Symbols
-term_num = 9
+t_nu = 9
 
-def rd_f(term_num):
-  return rd.randint(1,term_num)
+## Generate random well-formed-formulas
+def rd_f(t_nu):
+    return rd.randint(1,t_nu)
 
-symb = {"DE": [term_num + 1],
-        "BR": [term_num + 2],
-        "NO": [term_num + 3],
-        "TH": [term_num + 4],
-        "OR": [term_num + 5],
-        "AN": [term_num + 6],
-        "FA": [term_num + 7]}
+## Define numerical values for symbols:
+symb = {"DE": [t_nu + 1],                               # DERIVES
+        "BR": [t_nu + 2],                               # BRACKET
+        "NO": [t_nu + 3],                               # NOT
+        "TH": [t_nu + 4],                               # THEN
+        "OR": [t_nu + 5],                               # OR
+        "AN": [t_nu + 6],                               # AND
+        "FA": [t_nu + 7]}                               # FALSUM
 
-## Definition of rules 
-fa_e   = lambda prem: rd_f()
-no_e   = lambda prem: symb["FA"]
-no_i_a = lambda prem: symb["NO"] + prem[1]
-no_i_b = lambda prem: symb["NO"] + prem[0]
-an_i   = lambda prem: prem[0]    + symb["AN"] + prem[1]
-an_e_a = lambda prem: prem[0]
-an_e_b = lambda prem: prem[1]
-th_e_a = lambda prem: prem[0][2]
-th_e_b = lambda prem: prem[1][2]
-th_i   = lambda prem: prem[0]    + symb["TH"] + prem[1]
-or_i_a = lambda prem: prem[0]    + symb["OR"] + rd_f()
-or_i_b = lambda prem: rd_d()     + symb["OR"] + prem[0] 
+## Definition of rules of intuitionistic propositional logic (INPL):
+fa_e = lambda prem: rd_f()                              # FALSUM_ELIMINATION
+no_e = lambda prem: symb["FA"]                          # NOT_ELIMINATION
+n_ia = lambda prem: symb["NO"] + prem[1]                # NOT_INTRODUCTION_A
+n_ib = lambda prem: symb["NO"] + prem[0]                # NOT_INTRODUCTION_B 
+an_i = lambda prem: prem[0]    + symb["AN"] + prem[1]   # AND_INTRODUCTION
+a_ea = lambda prem: prem[0]                             # AND_ELIMINATION_A
+a_eb = lambda prem: prem[1]                             # AND_ELIMINATION_B
+t_ea = lambda prem: prem[0][2]                          # THEN_ELIMINATION_A
+t_eb = lambda prem: prem[1][2]                          # THEN_ELIMINATION_B
+th_i = lambda prem: prem[0]    + symb["TH"] + prem[1]   # THEN_INTRODUCTION
+o_ia = lambda prem: prem[0]    + symb["OR"] + rd_f()    # OR_INTRODUCTION_A
+o_ib = lambda prem: rd_d()     + symb["OR"] + prem[0]   # OR_INTRODUCTION_B
 
-## List the rules
-ipl = [fa_e, no_e, no_i_a, no_i_b, an_i, an_e_a,
-       an_e_b, th_e_a, th_e_b, th_i, or_i_a, or_i_b]
+## List the rules of INPL
+inpl = [fa_e, no_e, n_ia, n_ib, an_i, a_ea,
+       a_eb, t_ea, t_eb, th_i, o_ia, o_ib]
 
-## Check whether rules is applicable to premises
-def app_check(rule,
-              term_num = int,
-              prem = list):
-  if len(prem) == 1:
-    if rule == fa_e and symb["FA"] == prem[0]:
-      return True
-    if rule == an_e_a or rule == an_e_b:
-      if len(prem[0]) == 3:
-        if prem[0][1] == symb["AN"]:
-          return True
-    if rule == or_i_a or rule == or_i_b:
-      return True
-  if len(prem) == 2:
-    if symb["FA"] == prem[0] or symb["FA"] == prem[1]:
-      print("a")
-      if rule == no_i_a and prem[0] == symb["FA"] and prem[1] != symb["FA"]:
-        return True
-      if rule == no_i_b and prem[1] == symb["FA"] and prem[0] != symb["FA"]:
-        return True
-    else:
-      if rule == no_e:
-        if len(prem[0]) == 2 and len(prem[1]) == 1 and len(prem[0][0]) == 1:
-          if prem[0][1] == prem[1] and prem[0][0] == symb["NO"]:
-              return True
-        if len(prem[1]) == 2 and len(prem[0]) == 1 and len(prem[1][0]) == 1:
-          if prem[1][1] == prem[0] and prem[1][0] == symb["NO"]:
-              return True
-      if rule == an_i:
-          return True
-      if rule == th_e_a:
-        if len(prem[0]) == 3 and len(prem[1]) == 1:
-          if prem[0][1] == symb["TH"] and prem[0][0] == prem[1]:
+## Define check for applicability of rule to premises via syntactic conditions:
+def check(rule, prem = list):
+    if len(prem) == 1:
+        if rule == fa_e:                                # FALSUM_ELIMINATION
+            if symb["FA"] == prem[0]:
+                return True
+        if rule == a_ea or rule == a_eb:                # AND_ELIMINATION
+            if len(prem[0]) == 3:
+                if prem[0][1] == symb["AN"]:
+                    return True
+        if rule == o_ia or rule == o_ib:                # OR_INTRODUCTION_A and B
             return True
-      if rule == th_e_b:
-        if len(prem[1]) == 3:
-          if prem[1][1] == symb["TH"] and prem[1][0] == prem[0]:
-            return True
-      if rule == th_i:
-        return True
+    if len(prem) == 2:
+        if symb["FA"] == prem[0] or symb["FA"] == prem[1]:
+            if rule == n_ia:                            # NOT_INTRODUCTION_A
+                if prem[0] == symb["FA"]:
+                    if prem[1] != symb["FA"]:
+                        return True
+            if rule == n_ib                             # NOT_INTRODUCTION_B
+                if prem[1] == symb["FA"]:
+                    if prem[0] != symb["FA"]:
+                return True
+        else:
+            if rule == no_e:                            # NOT_ELIMINATION
+                if len(prem[0]) == 2:
+                    if len(prem[1]) == 1:
+                        if len(prem[0][0]) == 1:
+                            if prem[0][1] == prem[1]:
+                                if prem[0][0] == symb["NO"]:
+                                    return True
+                if len(prem[1]) == 2:
+                    if len(prem[0]) == 1:
+                        if len(prem[1][0]) == 1:
+                            if prem[1][1] == prem[0]:
+                                if prem[1][0] == symb["NO"]:
+                                    return True
+            if rule == an_i:                            # AND_INTRODUCTION
+                return True
+            if rule == t_ea:                            # THEN_ELIMINATION_A
+                if len(prem[0]) == 3:
+                    if len(prem[1]) == 1:
+                        if prem[0][1] == symb["TH"]:
+                            if prem[0][0] == prem[1]:
+                                return True
+            if rule == t_eb:                            # THEN_ELIMINATION_B
+                if len(prem[1]) == 3:
+                    if prem[1][1] == symb["TH"]:
+                        if prem[1][0] == prem[0]:
+                            return True
+            if rule == th_i:                            # THEN_INTRODUCTION
+                return True
--- generation.py
+++ generation.py
@@ -1,38 +1,30 @@
-"""Generates the premises."""
-def premises_gen(self):
-  arg_num = rd.randint(*span_arg_num)
-  arguments = rd.sample(terms, arg_num)
-  # Optionally some logic for building well formed formulas.
-  return arguments
+import calc
 
 """When used with the Derivation method it returns:
-  - X: The input to a network of the form [PREMISES, DELIMITER, CONCLUSION]
-  - y: The output to a network of the form
-    [PREMISES, DERIVATION STEP 1,.recursion_task.., DERIVATION STEP N, CONCLUSION]"""
-def dataset_builder(iter_range = list,
-                    sample_size = int):
-  X, y = [], []
-  bar = tqdm(total=sample_size, desc="Generating example derivations")
-  while len(X) < sample_size:
-    derivation = premises_gen()
-    if len(iter_range) == 2:
-      iterations = range(rd.randint(*iter_range))
-    else:
-      iterations = iter_range
-    premise = copy.copy(derivation)
-    for iteration in iterations:
-      if len(flatten(derivation)) >= 2:
-        args = rd.sample(flatten(derivation), rd.randint(1,2))
-      else:
-        args = rd.sample(flatten(derivation), 1)
-      derived, max_num = apply_nonterms(term_num, args)
-      derivation.append(derived)
-    conclusion = derivation[-1]
-    X_new = to_int(flatten([premise, term_num + 1, conclusion]))
-    #print(f"X_new:{X_new}")
-    y.append(to_int(flatten(derivation)))
+    - X: The input to a network of the form [PREMISES, DELIMITER, CONCLUSION]
+    - y: The output to a network of the form
+         [PREMISES, DERIVATION STEP 1,.recursion_task.., DERIVATION STEP N, CONCLUSION]"""
 
-    X.append(X_new)
-    bar.update(1)
-  bar.close()
-  return X, y, max_num
+def dat_b(itra = list, s_si = int):
+    inpt, outp = [], []
+    gbar = tqdm(total=s_si, desc="Generating example derivations")
+    while len(X) < s_si:
+        drva = []
+        for p in rd.randint(1, 3):
+            drva.append(calc.rd_f())
+        drva.append.(calc.symb["DE"])
+        outp = drva
+        for it in randint(itra):
+            pr_n = rd.randint(1, 2)
+            prem = rd.choice(drva, pr_n)
+            cand = []
+            for rule in calc.inpl:
+                if calc.check(rule, prem):
+                    cand.append(rule)
+            used = rd.choice(cand)
+            drva.append(used())
+        inpt.append(drva)
+        outp.append(drva[-1])
+        gbar.update(1)
+    gbar.close()
+    return inpt, outp
